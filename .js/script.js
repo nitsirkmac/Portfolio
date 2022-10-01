c570acfe88121d990e598d46f53aa0831a505f11
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-09-30 20:31:07 -0400

    more framework added

--- .js/script.js
+++ .js/script.js
@@ -1,1 +1,6 @@
 console.log("Hello Darkness")
+
+document.getElementById('name').placeholder = "Name"
+document.getElementById('email').placeholder = "Email"
+document.getElementById('message').placeholder = "Send me a message!"
+//make message box larger
