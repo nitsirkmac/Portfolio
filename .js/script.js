db9c84c6d6718778773d5aa8ae39dbafcfe28afb
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-10-04 22:15:26 -0400

    first push to live site

--- .js/script.js
+++ .js/script.js
@@ -1,5 +1,10 @@
 console.log("Hello Darkness")
 
+var menuLinks = [
+    { text: 'bio', href: '/bio'},
+    { text: 'catalog', href: '/catalog'},
+    { text: 'contact', href: '/contact'},
+]
 
 
 
@@ -9,3 +14,6 @@ document.getElementById('name').placeholder = "Name"
 document.getElementById('email').placeholder = "Email"
 document.getElementById('message').placeholder = "Send me a message!"
 //make message box larger
+
+// document.getElementById('#send')
+// function - send to my email address
