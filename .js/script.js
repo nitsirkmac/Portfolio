4e148abfcc4a3b0d2edc2d253005e5f5fe5c19b7
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-10-08 12:18:36 -0400

    fixing paths

--- .js/script.js
+++ .js/script.js
@@ -6,15 +6,4 @@ console.log("Hello Darkness")
 
 
 
-document.getElementById('name').placeholder = "Name"
-document.getElementById('email').placeholder = "Email"
-document.getElementById('message').placeholder = "Send me a message!"
-//make message box larger
-
-// document.getElementById('#send')
-// function - send to my email address
-
-// Nav-bar : hover over item to underline, click to move page to that section
-
-// animation - linedrawing lunar moth
 
